8fa91a6c420410ee0375043f7e4b89d44fd6be7a
BayanMuaddel <bayanmouddil@gmail.com>
2026-05-01 13:35:28 +0300

    setup github pages

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TodoList/',     // <-- هذا السطر هو يلي أضفناه
   plugins: [vue()],
+  base: '/todo-app-vue/' 
 })
